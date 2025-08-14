76c88a2332939faa0910e2ab2e850d581ada403e
ats <atsumitha@gmailcom>
2025-08-13 20:52:44 -0500

    js changed

---
+++ css/Javascript JS/Script.js
@@ -0,0 +1,7 @@
+document.querySelector('.menu-toggle').addEventListener('click',()=>{
+    document.querySelector('.nav-links').classList.toggle('active')
+})
+document.getElementById('contactForm').addEventListener('submit',()=>{
+    e.preventDefault();
+    alert("Thank you")
+})
